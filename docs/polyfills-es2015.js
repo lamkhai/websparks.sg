6e114159d8747ac44575c2bd8113f71afeaa6586
Lâm Khải <lk.lamkhai@gmail.com>
2021-08-07 11:45:24 +0700

    ng build

--- docs/polyfills-es2015.js
+++ docs/polyfills-es2015.js
@@ -6651,8 +6651,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\lklam\OneDrive\Tài liệu\GitHub\websparks\src\polyfills.ts */"./src/polyfills.ts");
-module.exports = __webpack_require__(/*! C:\Users\lklam\OneDrive\Tài liệu\GitHub\websparks\node_modules\@angular-devkit\build-angular\src\angular-cli-files\models\jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js");
+__webpack_require__(/*! C:\Users\lklam\OneDrive\Tài liệu\GitHub\websparks.sg\src\polyfills.ts */"./src/polyfills.ts");
+module.exports = __webpack_require__(/*! C:\Users\lklam\OneDrive\Tài liệu\GitHub\websparks.sg\node_modules\@angular-devkit\build-angular\src\angular-cli-files\models\jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js");
 
 
 /***/ })
